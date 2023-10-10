c33d6a2dd91141bab4c0c2f616a0323f4e5c32f5
Leclassique <75534147+Leclassique@users.noreply.github.com>
2023-10-10 20:49:11 +0300

    Completed task

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        if (first >= second && first >= third) {
+            System.out.println("The greatest number is: " + first);
+        } else if (second >= first && second >= third) {
+            System.out.println("The greatest number is: " + second);
+        } else {
+            System.out.println("The greatest number is: " + third);
+        }
     }
 }
